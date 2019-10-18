d535cab915e68d71b811ddc21363bb13f3a7b629
ivp <ivp@kondyli.ro>
2019-10-19 00:38:01 +0300

    Added javascrip code for palindrome.html

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
+// let Phrase = require("ivp-palindrome");
+
+// let string = prompt("Please enter a string for palindrome testing:");
+// let phrase = new Phrase(string);
+
+// if (phrase.palindrome()) {
+//   alert(`"${phrase.content}" is a palindrome!`);
+// } else {
+//   alert(`"${phrase.content}" is not a palindrome.`)
+// }
+
+
+
 let Phrase = require("ivp-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+function palindromeTester(event) {
+  event.preventDefault();
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`)
+  let phrase = new Phrase(event.target.phrase.value);
+  // let string = prompt("Please enter a string for palindrome testing:");
+  // let phrase = new Phrase(string);
+  let palindromeResult = document.querySelector("#palindromeResult");
+
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function(event) {
+    palindromeTester(event);
+  });
+});
